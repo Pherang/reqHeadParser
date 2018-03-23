7bb74259b32e4400f7f9cf13d3f1d41f1717e803
Pherang <pherang@users.noreply.github.com>
2018-03-23 12:38:24 -0400

    Added some basic routing of URL requests

--- reqheadparser.js
+++ reqheadparser.js
@@ -11,16 +11,22 @@ http.createServer( (req,res) => {
   res.on('error', (err) => {
     console.log(err)
   })
+ 
   
-  let ipaddress = forwarded(req)
-  let { headers } = req
-  let clientInfo = {}
-  let language = headers['accept-language'].split(',')
-  clientInfo.IP = ipaddress[(ipaddress.length-1)]
+  if (req.url === '/' && req.method === 'GET') {
+    res.end('Hello, connect to /api/whoami to get info about yourself')
+  }
+  if (req.url === '/api/whoami' && req.method === 'GET') {
+    let ipaddress = forwarded(req)
+    let { headers } = req
+    let clientInfo = {}
+    let language = headers['accept-language'].split(',')
+    clientInfo.IP = ipaddress[(ipaddress.length-1)]
 
-  let software = headers['user-agent'].match(/\(.*?\)/) || "no OS found"
-  clientInfo.software = software[0].substr(1,software[0].length-2)
-  clientInfo.language = language[0]
-  res.end(JSON.stringify(clientInfo))
+    let software = headers['user-agent'].match(/\(.*?\)/) || "no OS found"
+    clientInfo.software = software[0].substr(1,software[0].length-2)
+    clientInfo.language = language[0]
+    res.end(JSON.stringify(clientInfo))
+  }
 
 }).listen(PORT, () => console.log(`Listening on ${ PORT }`))
